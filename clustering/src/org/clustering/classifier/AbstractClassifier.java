feb23ec35954cdc69b5ba4c5e350cad644cbd1ea
zardosht <zardosht@dorosht>
2011-06-27 18:50:41 +0200

    imple recursive

--- clustering/src/org/clustering/classifier/AbstractClassifier.java
+++ clustering/src/org/clustering/classifier/AbstractClassifier.java
@@ -1,9 +1,10 @@
 package org.clustering.classifier;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
-import javax.naming.spi.DirStateFactory.Result;
+import java.util.Random;
+import java.util.Set;
 
 import org.clustering.model.Cluster;
 import org.clustering.model.Item;
@@ -12,6 +13,8 @@ public abstract class AbstractClassifier {
 
 	private final int numOfClusters;
 	private final List<Item> items;
+	private Set<Item> oldCentroids;
+	private Random random = new Random();
 
 	public AbstractClassifier(int numOfClusters, List<Item> items) {
 		this.numOfClusters = numOfClusters;
@@ -21,33 +24,69 @@ public abstract class AbstractClassifier {
 	
 	
 	public List<Cluster> createClusters(){
-		List<Item> centroids = chooseInitialCentroids();
-		return createClusters(centroids, new ArrayList<Cluster>());
+		List<Cluster> initialClusters = createInitialClusters();
+		return createClusters(initialClusters);
 	}
 	
-	private List<Item> chooseInitialCentroids() {
-		return new ArrayList<Item>(numOfClusters);
+	private List<Cluster> createInitialClusters() {
+		List<Cluster> initialClusters = new ArrayList<Cluster>();
+		Set<Item> initialCentroids = selectInitialCentroids();
+		for(Item centroid : initialCentroids){
+			Cluster cluster = new Cluster(centroid);
+			initialClusters.add(cluster);
+		}
+		return initialClusters;
 	}
 
 
-	public List<Cluster> createClusters(List<Item> centroids, List<Cluster> result){
-		if(isCentroidsChanging(centroids)){
-			return result;
+
+
+	private Set<Item> selectInitialCentroids() {
+		Set<Item> initialCentroids = new HashSet<Item>();
+		oldCentroids = new HashSet<Item>();		
+		while(initialCentroids.size() < numOfClusters){
+			int index = random.nextInt(items.size());
+			Item centroid = items.get(index);
+			initialCentroids.add(centroid);
+			oldCentroids.add(centroid);
+		}
+		return initialCentroids;
+	}
+
+
+
+	private List<Cluster> createClusters(List<Cluster> clusters){
+		if(!centroidsChanged(clusters)){
+			return clusters;
 		}else{
-			
+			for(Cluster cluster : clusters){
+				cluster.computeNewCnetroid();
+			}
+			double lastDist = Double.MAX_VALUE;
+			int newClusterIndex = 0;
+			for(Item item : items){
+				for(Cluster cluster : clusters){
+					double newDist = item.getDistance(cluster.getCentroid());
+					if(newDist < lastDist){
+						lastDist = newDist;
+						newClusterIndex = clusters.indexOf(cluster);
+					}
+				}
+				Cluster newCluster = clusters.get(newClusterIndex);
+				newCluster.addItem(item);
+			}
+			return createClusters(clusters);
 		}
 	}
 	
-	
-	
-	
-
-
-
-	protected abstract List<Item> chooseCentroids();
-
-
-	protected abstract boolean isCentroidsChanging(List<Item> centroids);
+	private boolean centroidsChanged(List<Cluster> clusters) {
+		Set<Item> newCentroids = new HashSet<Item>();
+		for(Cluster cluster : clusters){
+			newCentroids.add(cluster.getCentroid());
+		}
+		boolean result = !oldCentroids.equals(newCentroids);
+		return result;
+	}
 
 
 
--- clustering/src/org/clustering/model/Item.java
+++ clustering/src/org/clustering/model/Item.java
@@ -1,8 +1,18 @@
 package org.clustering.model;
 
 public class Item {
+	
+	private Cluster cluster; 
 
 	public double getDistance(Item item) {
 		return 0;
+	}
+
+	public Cluster getCluster() {
+		return cluster;
+	}
+
+	public void setCluster(Cluster cluster) {
+		this.cluster = cluster;
 	}
 }
