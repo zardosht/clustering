577a98b34964d026e807f5c3da12263ac89d7c63
zardosht <zardosht@dorosht>
2011-06-29 18:19:15 +0200

    bug with endless loop

--- clustering/src/org/clustering/model/Cluster.java
+++ clustering/src/org/clustering/model/Cluster.java
@@ -26,25 +26,23 @@ public class Cluster {
 
 	public void computeNewCnetroid() {
 		double[] distances = new double[members.size()];
-		for(int i =0; i < members.size(); i++) {
-			for(int j = i+1; j<members.size(); j++) {
+		for (int i = 0; i < members.size(); i++) {
+			for (int j = i + 1; j < members.size(); j++) {
 				double distance = members.get(i).getDistance(members.get(j));
-				distances[i]+=distance;
-				distances[j]+=distance;
+				distances[i] += distance;
+				distances[j] += distance;
 			}
 		}
 		int lastIndex = 0;
-		double smallestDistance = Double.MAX_VALUE;		
-		for(int i = 0; i < distances.length; i++) {
-			if(distances[i] < smallestDistance) {
+		double smallestDistance = Double.MAX_VALUE;
+		for (int i = 0; i < distances.length; i++) {
+			if (distances[i] < smallestDistance) {
 				smallestDistance = distances[i];
 				lastIndex = i;
 			}
 		}
 		Item newCentroid = members.get(lastIndex);
-		if(newCentroid != centroid){
-			centroidChanged = true;
-		}
+		centroidChanged = (newCentroid == centroid) ? false : true;
 		centroid = newCentroid;
 	}
 
@@ -55,8 +53,8 @@ public class Cluster {
 	public boolean contains(Item item) {
 		return members.contains(item);
 	}
-	
-	public boolean centroidChanged(){
+
+	public boolean centroidChanged() {
 		return centroidChanged;
 	}
 
