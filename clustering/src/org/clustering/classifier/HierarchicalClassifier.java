537a1896b67cfc9f11ae2294cbc956f2d1f5e1b9
Otto <wesendon@in.tum.de>
2011-07-21 12:38:06 +0200

    Change-Id: I9dd54fc0eb35310d8650c3e5602b4f2d1bef83d4

--- clustering/src/org/clustering/classifier/HierarchicalClassifier.java
+++ clustering/src/org/clustering/classifier/HierarchicalClassifier.java
@@ -9,35 +9,45 @@ import org.clustering.model.Item;
 
 public class HierarchicalClassifier {
 
-	public List<HierarchicalCluster> createHierarchicalCluster(List<Item> items) {
+	public HierarchicalCluster createHierarchicalCluster(List<Item> items) {
 
 		List<HierarchicalCluster> initialClusters = createInitialClusters(items);
 
-		mergeClusters(initialClusters);
-
-		return null;
+		while(initialClusters.size() > 1) {
+			initialClusters = mergeClusters(initialClusters);
+		}
+		
+		return initialClusters.get(0);
 	}
 
-
-	private HierarchicalCluster mergeClusters(List<HierarchicalCluster> initialClusters) {
-		int mI = 0, mJ = 0;
-		double maxSim = 0;
-		for (int i = 0; i < initialClusters.size(); i++) {
-			for (int j = i; j < initialClusters.size(); j++) {
-				double similarity = getSimilarity(initialClusters.get(i),
-						initialClusters.get(j));
-				if (similarity > maxSim) {
-					maxSim = similarity;
-					mI = i;
-					mJ = j;
+	private List<HierarchicalCluster> mergeClusters(
+			List<HierarchicalCluster> initialClusters) {
+		ArrayList<HierarchicalCluster> list = new ArrayList<HierarchicalCluster>();
+		while (initialClusters.size() > 1) {
+			int mI = 0, mJ = 0;
+			double maxSim = 0;
+			for (int i = 0; i < initialClusters.size(); i++) {
+				for (int j = i+1; j < initialClusters.size(); j++) {
+					double similarity = getSimilarity(initialClusters.get(i),
+							initialClusters.get(j));
+					if (similarity > maxSim) {
+						maxSim = similarity;
+						mI = i;
+						mJ = j;
+					}
 				}
 			}
+			HierarchicalCluster iCluster = initialClusters.get(mI);
+			HierarchicalCluster jCluster = initialClusters.get(mJ);
+			// remove j first, because j is always behind i
+			initialClusters.remove(mJ);
+			initialClusters.remove(mI);
+			list.add(new HierarchicalCluster(maxSim, iCluster, jCluster));
 		}
-		HierarchicalCluster iCluster = initialClusters.get(mI);
-		HierarchicalCluster jCluster = initialClusters.get(mJ);
-		initialClusters.remove(mJ);
-		initialClusters.remove(mI);
-		return new HierarchicalCluster(maxSim,iCluster,jCluster);
+		if(initialClusters.size() == 1) {
+			list.add(initialClusters.get(0));
+		}
+		return list;
 	}
 
 	private double getSimilarity(HierarchicalCluster firstCluster,
@@ -48,7 +58,7 @@ public class HierarchicalClassifier {
 	private List<HierarchicalCluster> createInitialClusters(List<Item> items) {
 		ArrayList<HierarchicalCluster> result = new ArrayList<HierarchicalCluster>();
 		for (Item item : items) {
-			result.add(new HierarchicalCluster(1, item));
+			result.add(new HierarchicalCluster(item));
 		}
 		return result;
 	}
--- clustering/src/org/clustering/model/HierarchicalCluster.java
+++ clustering/src/org/clustering/model/HierarchicalCluster.java
@@ -1,15 +1,46 @@
 package org.clustering.model;
 
+import java.util.HashSet;
+
 public class HierarchicalCluster {
 
 	private double simLevel;
+	private Item item;
+	private HierarchicalCluster firstChild;
+	private HierarchicalCluster secondChild;
+	private HierarchicalCluster parent;
 
 	public HierarchicalCluster(Item initialCentoroid) {
+		this.item = initialCentoroid;
 		simLevel = 1;
 	}
 
-	public HierarchicalCluster(double maxSim, HierarchicalCluster hierarchicalCluster,
-			HierarchicalCluster hierarchicalCluster2) {
+	public HierarchicalCluster(double maxSim, HierarchicalCluster cluster1,
+			HierarchicalCluster cluster2) {
+				this.simLevel = maxSim;
+				this.firstChild = cluster1;
+				this.secondChild = cluster2;
+				// set parent
+				this.firstChild.setParent(this);
+				this.secondChild.setParent(this);
+	}
+
+	private void setParent(HierarchicalCluster hierarchicalCluster) {
+		this.parent = hierarchicalCluster;
+	}
+	
+	public void getItems() {
+		HashSet<Item> children = new HashSet<Item>();
+		getChildren(children);
+	}
+
+	private void getChildren(HashSet<Item> children) {
+		if(item != null) {
+			children.add(item);
+			return;
+		}
+		firstChild.getChildren(children);
+		secondChild.getChildren(children);
 	}
 
 }
