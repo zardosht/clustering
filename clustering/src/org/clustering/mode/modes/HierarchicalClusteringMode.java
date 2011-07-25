235ed4859fc4e673f4a282557dd9808751f55341
zardosht <zardosht@dorosht>
2011-07-25 16:25:09 +0200

    hac usage

--- clustering/src/org/clustering/mode/modes/HierarchicalClusteringMode.java
+++ clustering/src/org/clustering/mode/modes/HierarchicalClusteringMode.java
@@ -18,7 +18,7 @@ import org.clustering.util.VisualisationUtil;
 public class HierarchicalClusteringMode extends AbstractMode {
 
 	public HierarchicalClusteringMode() {
-		super("-hierarchical", "HAC");
+		super("-hierarchical", "Runs a hierarchical clustering and creates a dendrogram of all items:\njava -jar clustering.jar -hierachical");
 	}
 
 	public void _runCreateCluster() throws Exception {
